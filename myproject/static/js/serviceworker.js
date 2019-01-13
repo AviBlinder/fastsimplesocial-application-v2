eb4476c8393a28dacaffeea38e55073da328373d
Avi Blinder <aviblinder@gmail.com>
2019-01-13 20:26:32 +0000

    varsToCache in ws.js

--- myproject/static/js/serviceworker.js
+++ myproject/static/js/serviceworker.js
@@ -1,10 +1,12 @@
 var staticCacheName = 'djangopwa-v1';
  
+var filesToCache = [ '/' ]
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        OFFLINE_URL
+        filesToCache
       ]);
     })
   );
